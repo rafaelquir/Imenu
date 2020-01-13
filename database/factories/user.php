ed665796e0df1750e8041c046715a39d7335b08f
Iván Obejo <ivanobejo@hotmail.es>
2020-01-13 17:30:25 +0100

    populate de DataBase

---
+++ database/factories/user.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use Illuminate\Support\Str;
+use App\Model;
+use App\User;
+use Faker\Generator as Faker;
+
+$factory->define(User::class, function (Faker $faker) {
+   return[
+
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'lastName' => $faker->name,
+        'password' => bcrypt('password'),
+        'api_token' => bcrypt('password'),
+    ];
+});
