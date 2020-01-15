8abda339c0c13d1b593b83bcdaa939ce542a7db1
Iván Obejo <ivanobejo@hotmail.es>
2020-01-15 19:48:19 +0100

    login de usuario para pasar la información necesaria

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,19 @@ Route::middleware('auth:api')->get('/user/{id}', function (Request $request) {
     return User::find($request->id);
 });
 Route::post('/user/create', 'Auth\RegisterController@create');
+
+Route::get('/login', function (Request $request) {
+    $user = User::find($request->email);
+    if ($request->password == Hash::check('plain-text', $user->password)) {
+        $loginUser = [
+            'serverRequest'=> 200,
+            'name'=>$user->name,
+            'lastName'=>$user->lastName,
+            'api_token'=>$user->api_token,
+        ];
+        return $loginUser;
+    }else{
+        return 400;
+    }
+
+});
