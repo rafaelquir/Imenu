a3b9bea3d5db75015bf11c5c78b47fafe68a6d85
Pedro <pedro_soto_aaps1ta1819@cev.com>
2020-01-22 20:32:22 +0100

    [PS][Home - Api]
    RestauranteController y Arreglo final UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,12 +10,13 @@ class UserController extends Controller
 {
     protected function delete($id){
         $response = array('error_code' => 404, 'error_msg' => 'User '.$id.' not found');
-        $user = User::find($id);
+        $user = User::where('id', $id)
+            ->get()[0];
 
         if (!empty($user)) {
             try{
                 $user->delete();
-                $response = array('error_code' => 200, 'error_msg' => '');
+                $response = array('error_code' => 200, 'error_msg' => 'OK');
                 Log::info('User delete');
 
             } catch (\Exception $e) {
@@ -31,37 +32,19 @@ class UserController extends Controller
 
     protected function update(Request $request, $id){
         $response = array('error_code' => 400, 'error_msg' => 'User '.$id.' not found');
-        $user = User::find($id);
+        $user = User::where('id', $id)
+            ->get()[0];
 
-        if (!empty($user)) {
-            $ok = true;
-
-            if (isset($request->name) || isset($request->lastName)) {
-
-                if (!empty($request->name)) {
-                    $user->name = ucfirst(strtolower($request->name));
-                }
-
-                if (!empty($request->lastName)) {
-                    $user->lastName = ucfirst(strtolower($request->lastName));
-                }
-
-            } else {
-                $ok = false;
-                $response['error_msg'] = 'No changes made';
+        if (isset($request) && isset($id) && !empty($user)) {
+            try {
+                $user->name = $request->name ? ucfirst(strtolower($request->name)) : $user->name;
+                $user->lastName =$request->lastName ? ucfirst(strtolower($request->lastName)) : $user->lastName;
+                $user->save();
+                $response = array('error_code' => 200, 'error_msg' => 'OK');
+            } catch (\Exception $e) {
+                Log::alert('Function: Update User, Message: '.$e);
+                $response = array('error_code' => 500, 'error_msg' => "Server connection error");
             }
-
-            if ($ok) {
-                try {
-                    $user->save();
-                    $response = array('error_code' => 200, 'error_msg' => 'OK');
-                } catch (\Exception $e) {
-                    Log::alert('Function: Update User, Message: '.$e);
-                    $response = array('error_code' => 500, 'error_msg' => "Server connection error");
-                }
-            }
-        }else {
-            $response = array('error_code' => 404, 'error_msg' => 'User '.$id.' not found');
         }
 
         return response()->json($response);
